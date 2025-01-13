b96f22a38f77e7b31bfe84b8b8b4bb645ab161af
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-13 19:47:29 +0100

    Update

--- Keyhelp.php
+++ Keyhelp.php
@@ -97,7 +97,7 @@ class Keyhelp extends Server
     private function getRequest($url): \GuzzleHttp\Promise\PromiseInterface|\Illuminate\Http\Client\Response
     {
         $response = Http::withHeaders([
-            'X-API-Key: ' . $this->config('apiKey'),
+            'X-API-KEY:' . $this->config('apiKey'),
             'Content-Type: application/json',
         ])->get($url);
 

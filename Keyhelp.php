f3a17376a10f323f9efa64bad48a1ff770bf66b5
Alexander Bergmann <152819279+evarioooo@users.noreply.github.com>
2025-01-15 16:08:15 +0100

    Update Keyhelp.php

--- Keyhelp.php
+++ Keyhelp.php
@@ -151,7 +151,6 @@ class Keyhelp extends Server
      */
     public function createServer($user, $params, $order, $orderProduct, $configurableOptions)
     {
-        dd($params);
         $sanitized = preg_replace('/[^a-zA-Z0-9]/', '', strtolower($user->username));
         $json = [
             'username' =>  $sanitized,
